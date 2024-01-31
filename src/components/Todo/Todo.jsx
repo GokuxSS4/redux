a06e068dd4b6563f2608acd71fb9c04abf0ca719
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-01-31 18:54:34 +0530

    edit todo added

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,14 +1,27 @@
 import { useDispatch } from "react-redux"
 import { bindActionCreators } from "redux";
-import {removeTodo} from '../../actions/index'
+import {removeTodo,editTodo} from '../../actions/index'
+import { useState } from "react";
 
 export default function Todo({title,id}) {
+  const [isEdit,setIsEdit] = useState(false);
+  const [editText,setEditText] = useState(title);
+
   const dispatch = useDispatch();
-  const action = bindActionCreators({removeTodo},dispatch);
+  const action = bindActionCreators({removeTodo,editTodo},dispatch);
+  
+  function editTodoItem(){
+    if(isEdit){
+      action.editTodo({id:id,title:editText});
+    }
+    setIsEdit(!isEdit);
+  }
+
   return (
     <div>
-      {title}
+      {isEdit ? <input value={editText} onChange={(e)=>setEditText(e.target.value)} placeholder="Edit todo.." /> : title }
       <button onClick={()=>action.removeTodo(id)}>Delete</button>
+      <button onClick={editTodoItem}>{isEdit?"SAVE":"EDIT"}</button>
     </div>
   )
 }
